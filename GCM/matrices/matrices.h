f762148d02bc471d9370b27efa3943c3ac264a32
TynK-M <matteo.pauroso@gmail.com>
2025-10-09 19:31:25 +0200

    add lib pre: create matrices.h Created matrices.h to start working

---
+++ GCM/matrices/matrices.h
@@ -0,0 +1,39 @@
+/** 
+ * This file is part of a studying library.
+ * As said it is not meant to be optimal or well-integrated with all the systems.
+ * 
+ * If someone is deciding for a random reason to use it, and founds a problem.
+ * Please open an issue.
+ */
+
+#ifndef GCM_MATRICES
+
+#define GCM_MATRICES
+
+
+
+#endif // GCM_MATRICES
+
+/**
+ * MIT License
+ *
+ * Copyright (c) 2025 Matteo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
